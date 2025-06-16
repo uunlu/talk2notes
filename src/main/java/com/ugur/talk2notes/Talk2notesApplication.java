0dbfee42045985e6d3fbe78b02c8df2dd6675614
uunlu <vally10@live.com>
2025-06-16 09:50:56 +0200

    Initial commit

---
+++ src/main/java/com/ugur/talk2notes/Talk2notesApplication.java
@@ -0,0 +1,12 @@
+package com.ugur.talk2notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talk2notesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Talk2notesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ugur/talk2notes/Talk2notesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugur.talk2notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talk2notesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
